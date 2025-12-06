6792794ad7f6dd67eb040038a391caf37c4d9529
JoonChicken <jbsquirrel06@gmail.com>
2025-12-06 13:13:39 -0500

    Generate header for all pages

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,6 +1,20 @@
-
-//CODE TO LOAD FOOTER
+// Prepend navbar
 document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('header-container').innerHTML = `
+        <!-- Navbar -->
+        <nav class="topnav" id="myTopnav">
+            <a href="index.html" class="logo">SkillSwap</a>
+            <div class="nav-links">
+                <a href="account.html">Account</a>
+                <a href="members.html">Members</a>
+                <a href="contact.html">Contact Us</a>
+                <a href="signup.html" class="active">Sign Up</a>
+            </div>
+            <a class="icon" onclick="toggleMenu()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </nav>    
+    `;
     document.getElementById('footer-container').innerHTML = `
         <footer class="site-footer">
             <p>&copy; 2025 SkillSwap. All rights reserved.</p>
@@ -10,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
         </footer>
     `;
 });
-//CODE TO LOAD FOOTER
+
 
 //CODE TO LOAD MEMBERS FROM DATABASE
 document.addEventListener("DOMContentLoaded", () => {
